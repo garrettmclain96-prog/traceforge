47b11b3a84c817be96580c39053e6b2b0dc2c0c1
garrettmclain96-prog <garrett.mclain96@icloud.com>
2026-09-24 08:30:10 -0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='traceforge-v1';
+const ASSETS=['./','./index.html','./styles-base.css','./styles-responsive.css','./app-core-1.js','./app-core-2.js','./app-cases-1.js','./app-cases-2.js','./app-ui-1.js','./app-ui-2a.js','./app-ui-2b.js','./app-wire.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
