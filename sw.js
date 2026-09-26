e5b686a3ec36c3b8860f0422f177c31803c886e6
garrettmclain96-prog <garrett.mclain96@icloud.com>
2026-09-25 20:20:24 -0500

    chore: bust PWA cache after recovery repair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='traceforge-v3.2';
+const CACHE='traceforge-v3.3';
 const ASSETS=['./','./index.html','./styles-base.css','./styles-responsive.css','./styles-recovery.css','./styles-catalog.css','./app-core-1.js','./app-core-2.js','./app-cases-1.js','./app-cases-2.js','./app-ui-1.js','./app-ui-2a.js','./app-ui-2b.js','./app-recovery.js','./app-easter.js','./app-source-catalog.js','./app-wire.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
